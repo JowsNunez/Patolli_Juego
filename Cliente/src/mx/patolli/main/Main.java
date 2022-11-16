4fb1f6622988737faae33482dbdbb0b234931550
Jose N <jose.nunez181930@potros.itson.edu.mx>
2022-11-15 21:39:44 -0700

    Proyect nb y intelli Cliente

---
+++ Cliente/src/mx/patolli/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.patolli.main;
+
+/**
+ *
+ * @author el_fr
+ */
+public class Main {
+   public static void main(String[] args){
+      System.out.println("Hello world");
+   }
+    
+}
